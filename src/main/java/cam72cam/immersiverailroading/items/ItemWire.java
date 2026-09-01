20b497e79ec8861f61354c3b2a8a3d3959a01f60
poizzy <janis@golombek.at>
2026-09-01 04:21:01 +0200

    [feat] Added possibility to define more connectors on a mast

--- src/main/java/cam72cam/immersiverailroading/items/ItemWire.java
+++ src/main/java/cam72cam/immersiverailroading/items/ItemWire.java
@@ -2,9 +2,11 @@ package cam72cam.immersiverailroading.items;
 
 import cam72cam.immersiverailroading.ImmersiveRailroading;
 import cam72cam.immersiverailroading.registry.DefinitionManager;
+import cam72cam.immersiverailroading.registry.MastDefinition;
 import cam72cam.immersiverailroading.registry.WireDefinition;
 import cam72cam.immersiverailroading.tile.TileMast;
 import cam72cam.mod.entity.Player;
+import cam72cam.mod.entity.boundingbox.IBoundingBox;
 import cam72cam.mod.item.ClickResult;
 import cam72cam.mod.item.CreativeTab;
 import cam72cam.mod.item.CustomItem;
@@ -15,11 +17,8 @@ import cam72cam.mod.net.Packet;
 import cam72cam.mod.serialization.TagField;
 import cam72cam.mod.util.Facing;
 import cam72cam.mod.world.World;
-import net.minecraft.tileentity.TileEntity;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static cam72cam.immersiverailroading.library.ChatText.*;
 
@@ -29,6 +28,53 @@ public class ItemWire extends CustomItem {
         super(ImmersiveRailroading.MODID, "item_wire");
     }
 
+    @Override
+    public void onClickAir(Player player, World world, Player.Hand hand) {
+        super.onClickAir(player, world, hand);
+        List<TileMast> masts = world.getBlockEntities(TileMast.class);
+        TileMast nearest = masts.stream().sorted(Comparator.comparingDouble(te -> player.getPosition().distanceToSquared(new Vec3d(te.getPos())))).toList().getFirst();
+        if (nearest == null) {
+            return;
+        }
+        MastDefinition def = nearest.getDefinition();
+        for (Map.Entry<String, Vec3d> connector : def.connectorPos.entrySet()) {
+            List<String> name = Collections.singletonList(connector.getKey());
+
+            Vec3d localEyes = player.getPositionEyes()
+                    .subtract(new Vec3d(nearest.getPos()))
+                    .subtract(0.5, 0, 0.5)
+                    .rotateYaw(-nearest.getAngle());
+            Vec3d localLook = player.getLookVector().rotateYaw(-nearest.getAngle());
+
+            IBoundingBox box = IBoundingBox.from(def.model.minOfGroups(name), def.model.maxOfGroups(name));
+            if (box.intersectsSegment(localEyes, localEyes.add(localLook.scale(10)))) {
+                ItemStack stack = player.getHeldItem(hand);
+                Data data = new Data(stack);
+
+                if (data.firstMast == null) {
+                    data.firstMast = nearest.getPos();
+                    data.firstDim = world.getId();
+                    data.firstConnector = name.getFirst();
+                    data.write();
+                    return;
+                }
+
+                if (nearest.getPos().equals(data.firstMast)) return;
+
+                if (data.firstDim != world.getId()) {
+                    player.sendMessage(WIRE_DIM_MISMATCH.getMessage());
+                    return;
+                }
+
+                nearest.addWire(data.firstMast, data.defID, data.firstConnector, name.getFirst());
+                new WirePacket(nearest.getPos(), data.defID, data.firstMast, data.firstConnector, name.getFirst()).sendToAll();
+                clearData(data);
+                break;
+            }
+        }
+    }
+
+    // TODO remove
     @Override
     public ClickResult onClickBlock(Player player, World world, Vec3i pos, Player.Hand hand, Facing facing, Vec3d inBlockPos) {
         if (world.isClient) {
@@ -60,8 +106,8 @@ public class ItemWire extends CustomItem {
             return ClickResult.REJECTED;
         }
 
-        clicked.addWire(data.firstMast, data.defID);
-        new WirePacket(clicked.getPos(), data.defID, data.firstMast).sendToAll();
+        clicked.addWire(data.firstMast, data.defID, "", "");
+        new WirePacket(clicked.getPos(), data.defID, data.firstMast, "", "").sendToAll();
         clearData(data);
         return ClickResult.ACCEPTED;
     }
@@ -103,18 +149,24 @@ public class ItemWire extends CustomItem {
         public Vec3i firstMast;
         @TagField
         public Vec3i secondMast;
+        @TagField
+        public String firstConnector;
+        @TagField
+        public String secondConnector;
 
         public WirePacket(){}
 
-        public WirePacket(Vec3i secondMast, String def, Vec3i firstMast) {
+        public WirePacket(Vec3i secondMast, String def, Vec3i firstMast, String firstConnector, String secondConnector) {
             this.def = def;
             this.firstMast = firstMast;
             this.secondMast = secondMast;
+            this.firstConnector = firstConnector;
+            this.secondConnector = secondConnector;
         }
 
         @Override
         protected void handle() {
-            getWorld().getBlockEntity(secondMast, TileMast.class).addWire(firstMast, def);
+            getWorld().getBlockEntity(secondMast, TileMast.class).addWire(firstMast, def, firstConnector, secondConnector);
         }
     }
 
@@ -130,6 +182,8 @@ public class ItemWire extends CustomItem {
         public String defID;
         @TagField
         public Integer firstDim;
+        @TagField
+        public String firstConnector;
 
         public Data(ItemStack stack) {
             super(stack);
--- src/main/java/cam72cam/immersiverailroading/render/block/MastModel.java
+++ src/main/java/cam72cam/immersiverailroading/render/block/MastModel.java
@@ -1,27 +1,57 @@
 package cam72cam.immersiverailroading.render.block;
 
+import cam72cam.immersiverailroading.IRItems;
+import cam72cam.immersiverailroading.ImmersiveRailroading;
 import cam72cam.immersiverailroading.registry.MastDefinition;
 import cam72cam.immersiverailroading.tile.OverheadWire;
 import cam72cam.immersiverailroading.tile.TileMast;
-import cam72cam.mod.model.common.mesh.Model;
+import cam72cam.mod.MinecraftClient;
+import cam72cam.mod.entity.Player;
+import cam72cam.mod.entity.boundingbox.IBoundingBox;
+import cam72cam.mod.math.Vec3i;
 import cam72cam.mod.render.StandardModel;
 import cam72cam.mod.render.common.ModelRenderer;
 import cam72cam.mod.render.opengl.BlendMode;
 import cam72cam.mod.render.opengl.Texture;
+import cam72cam.mod.resource.Identifier;
 
-import java.util.List;
+import java.util.*;
 
 public class MastModel {
     public static StandardModel getModel(TileMast tile) {
         StandardModel model = new StandardModel();
+        Texture connector = Texture.wrap(new Identifier(ImmersiveRailroading.MODID, "textures/connector.png"));
+        List<Vec3i> placedBlocks = new ArrayList<>();
+
+        Map<String, IBoundingBox> boundingBoxes = new HashMap<>();
+        MastDefinition def = tile.getDefinition();
+
+        for (String con : def.connectorPos.keySet()) {
+            List<String> singleton = Collections.singletonList(con);
+            boundingBoxes.put(con, IBoundingBox.from(def.model.minOfGroups(singleton), def.model.maxOfGroups(singleton)));
+        }
 
         model.addCustom(((renderState, v) -> {
             renderState.translate(0.5, 0, 0.5);
             renderState.rotate(tile.getAngle(), 0, 1, 0);
-            MastDefinition def = tile.getDefinition();
-            List<String> toBeRendered = def.model.groups().stream().filter(g -> !g.contains("CONNECTION_")).toList();
+            List<String> toBeRendered = def.model.groups().stream().filter(g -> !g.contains("CONNECTOR_")).toList();
+            List<String> connectors = def.model.groups().stream().filter(g -> g.contains("CONNECTOR_")).toList();
             try (ModelRenderer.Binding bound = ModelRenderer.getRendererFor(def.model).bind(renderState)) {
                 bound.enqueueOpaque(toBeRendered);
+
+                Player player = MinecraftClient.getPlayer();
+                if (player.getHeldItem(Player.Hand.PRIMARY).is(IRItems.ITEM_WIRE)) {
+                    bound.enqueueTransparent(connectors, state -> state.texture(connector).blend(new BlendMode(BlendMode.GL_SRC_ALPHA, BlendMode.GL_ONE_MINUS_SRC_ALPHA)).lighting(false));
+                }
+
+                List<Vec3i> removed = new ArrayList<>();
+
+                for (Vec3i placed : placedBlocks) {
+                    tile.getWorld().breakBlock(placed, false);
+                    removed.add(placed);
+                }
+                placedBlocks.removeAll(removed);
+
             }
         }));
 
