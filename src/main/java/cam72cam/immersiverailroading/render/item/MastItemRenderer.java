828ac300797883fbbb9db03f1e8938027f466c4f
poizzy <janis@golombek.at>
2026-09-07 19:44:11 +0200

    [fix] Snapping now always takes the actual middle

--- src/main/java/cam72cam/immersiverailroading/render/item/MastItemRenderer.java
+++ src/main/java/cam72cam/immersiverailroading/render/item/MastItemRenderer.java
@@ -1,12 +1,13 @@
 package cam72cam.immersiverailroading.render.item;
 
 import cam72cam.immersiverailroading.items.ItemMast;
+import cam72cam.immersiverailroading.library.TrackItems;
 import cam72cam.immersiverailroading.registry.DefinitionManager;
 import cam72cam.immersiverailroading.registry.MastDefinition;
 import cam72cam.immersiverailroading.render.block.MastModel;
-import cam72cam.immersiverailroading.thirdparty.trackapi.IRPathingData;
-import cam72cam.immersiverailroading.thirdparty.trackapi.ITrack;
+import cam72cam.immersiverailroading.tile.TileRail;
 import cam72cam.immersiverailroading.tile.TileRailBase;
+import cam72cam.immersiverailroading.track.*;
 import cam72cam.immersiverailroading.util.BlockUtil;
 import cam72cam.immersiverailroading.util.VecUtil;
 import cam72cam.mod.entity.Player;
@@ -37,7 +38,7 @@ public class MastItemRenderer implements ItemRender.IItemModel {
         });
     }
 
-    public static void renderMouseover(Player player, ItemStack stack, Vec3i pos, Vec3d ignoredVec, RenderState state, float ignoredPartialTicks) {
+    public static void renderMouseover(Player player, ItemStack stack, Vec3i pos, Vec3d hit, RenderState state, float ignoredPartialTicks) {
         World world = player.getWorld();
         ItemMast.Data data = new ItemMast.Data(stack);
         float localOff = data.getDistance();
@@ -45,23 +46,17 @@ public class MastItemRenderer implements ItemRender.IItemModel {
         float rotation = (-(Math.round(player.getRotationYawHead() / 15) * 15) - 90);
 
         if (BlockUtil.isIRRail(world, pos)) {
-            float yaw = player.getRotationYawHead();
-
             TileRailBase rail = world.getBlockEntity(pos, TileRailBase.class);
-            Vec3d vec = new Vec3d(0.5, 0, 0).rotateYaw(yaw);
-            IRPathingData next = new IRPathingData(renderOff.add(vec), 0);
-            rail.getNextPosition(next, VecUtil.fromWrongYaw(0.1, yaw), rail.getRenderGauge());
-            Vec3d firstPos = next.getUMCPos();
-            rail.getNextPosition(next, VecUtil.fromWrongYaw(-0.1, yaw), rail.getRenderGauge());
+            TileRail parent = rail instanceof TileRail tr ? tr : rail.getParentTile();
 
-            int offset = 2;
-            Vec3d umcPos = next.getUMCPos();
+            VecYPR onTrack = parent != null && parent.info != null ? getClosestPointOnTrack(world, parent, hit) : null;
+            if (onTrack != null) {
+                int offset = 2;
 
-            rotation = VecUtil.toYaw(umcPos.subtract(firstPos));
-
-            Vec3d off = new Vec3d(offset + localOff, 0, 0).rotateYaw(rotation);
-
-            renderOff = new Vec3d(new Vec3i(umcPos)).add(off);
+                rotation = rightSideYaw(onTrack.getYaw(), player.getRotationYawHead());
+                Vec3d off = VecUtil.fromYaw(offset + localOff, rotation);
+                renderOff = new Vec3d(new Vec3i(onTrack.x, onTrack.y, onTrack.z)).add(off);
+            }
         }
 
 
@@ -71,5 +66,61 @@ public class MastItemRenderer implements ItemRender.IItemModel {
         renderOff = renderOff.add(0.5, 0, 0.5).subtract(cameraPos);
 
         MastModel.renderMast(renderOff, state, model, rotation);
+    }
+
+    private static VecYPR getClosestPointOnTrack(World world, TileRail rail, Vec3d hit) {
+        BuilderBase builder = rail.info.getBuilder(world);
+        if (builder instanceof BuilderSwitch) {
+            builder = rail.info.withSettings(mutable -> mutable.type = TrackItems.STRAIGHT).getBuilder(world);
+        }
+
+        if (!(builder instanceof BuilderCubicCurve curveBuilder)) {
+            return null;
+        }
+
+        CubicCurve curve = curveBuilder.getCurve();
+
+        Vec3d railOffset = rail.info.placementInfo.placementPosition.add(rail.getPos());
+        Vec3d localHit = hit.subtract(railOffset);
+
+        double t = closestT(curve, localHit);
+        Vec3d worldPos = curve.position(t).add(railOffset);
+        float yaw = VecUtil.toYaw(curve.derivative(t));
+        return new VecYPR(worldPos, yaw);
+    }
+
+    private static double closestT(CubicCurve curve, Vec3d target) {
+        int coarseSamples = 64;
+        double bestT = 0;
+        double bestDistSq = Double.MAX_VALUE;
+        for (int i = 0; i <= coarseSamples; i++) {
+            double t = i / (double) coarseSamples;
+            double distSq = curve.position(t).distanceToSquared(target);
+            if (distSq < bestDistSq) {
+                bestDistSq = distSq;
+                bestT = t;
+            }
+        }
+
+        double lo = Math.max(0, bestT - 1.0 / coarseSamples);
+        double hi = Math.min(1, bestT + 1.0 / coarseSamples);
+        for (int i = 0; i < 30; i++) {
+            double m1 = lo + (hi - lo) / 3;
+            double m2 = hi - (hi - lo) / 3;
+            if (curve.position(m1).distanceToSquared(target) <= curve.position(m2).distanceToSquared(target)) {
+                hi = m2;
+            } else {
+                lo = m1;
+            }
+        }
+        return (lo + hi) / 2;
+    }
+
+    private static float rightSideYaw(float trackYaw, float playerYawHead) {
+        Vec3d perpA = VecUtil.fromYaw(1, trackYaw + 90);
+        Vec3d perpB = VecUtil.fromYaw(1, trackYaw - 90);
+        Vec3d playerRight = VecUtil.fromWrongYaw(1, playerYawHead + 90);
+
+        return perpA.dotProduct(playerRight) >= perpB.dotProduct(playerRight) ? trackYaw + 90 : trackYaw - 90;
     }
 }
